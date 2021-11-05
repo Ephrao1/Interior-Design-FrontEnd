f9a77617d58484d71c67cc0f474abdb88707be58
Ephraim Bundi <ebundi04@gmail.com>
2021-11-05 03:00:20 +0300

    Modify form

--- src/app/companyregister/companyregister.component.ts
+++ src/app/companyregister/companyregister.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserAuthService } from '../user-auth.service';
 
 @Component({
   selector: 'app-companyregister',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyregisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: UserAuthService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showSuccessMessage = false;
+
+  // create a new user
+  onSubmit(form: NgForm) {
+    const username = form.value.username;
+    const email = form.value.email;
+    const password = form.value.password;
+    const c_password = form.value.c_password;
+
+    // check if passwords match
+    if (password !== c_password) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    this.authService
+      .createUser(username, email, password)
+      .subscribe((response) => {
+        console.log(response);
+        this.showSuccessMessage = true;
+      });
+
+    form.reset();
+
   }
-
 }
